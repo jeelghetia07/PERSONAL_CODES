96d20e1ed7253426c05e41ac71897eba3d017802
jeelghetia07 <jeelghetia07@gmail.com>
2025-08-16 07:49:44 +0530

    daily practice day27

--- TUF DSA/3_Arrays/MEDIUM/Majority_half.cpp
+++ TUF DSA/3_Arrays/MEDIUM/Majority_half.cpp
@@ -7,9 +7,25 @@ using namespace std;
 
 class Majority{
 public:
-
+    int majorityElement(vector<int>& arr){
+        int n = arr.size();
+        for(int i = 0 ; i < n ; i++){
+            int count = 0;
+            for(int j = 0 ; j < n ; j++){
+                if(arr[i] == arr[j]){
+                    count++;
+                }
+            }
+            if(count > (n/2)) return arr[i];
+        }
+        return 0;
+    }
 };
 
 int main(){
+    vector<int> arr = {1,2,0,1,2,1,2,2,2};
+    Majority m;
+    int ans = m.majorityElement(arr);
+    cout << "the ans is : " << ans << endl;
     return 0;
 }
