1e97ccc1a61c93dcb6d4c0b5c7a2ed099e412d7e
jeelghetia07 <jeelghetia07@gmail.com>
2026-01-07 11:46:52 +0530

    solved graphs and DP

---
+++ TUF DSA/16_DP/2D_problems/Ninja_Training.cpp
@@ -0,0 +1,89 @@
+#include<bits/stdc++.h>
+using namespace std;
+
+// int f(int day, int last, vector<vector<int>> &points, vector<vector<int>> &dp){
+//     if(day == 0){
+//         int maxi = 0;
+//         for(int task = 0 ; task < 3 ; task++){
+//             if(task != last){
+//                 maxi = max(maxi, points[0][task]);    // here pinits[0][task]  becoz we are talking only about last ind 0 row...so [0][task].
+//             }
+//         }
+//         return maxi;
+//     }
+
+//     if(dp[day][last] != -1)return dp[day][last];
+
+//     int maxi = 0;
+//     for(int task = 0 ; task < 3 ; task++){
+//         if(task != last){
+//             int point = points[day][task] + f(day-1,task,points, dp);
+//             maxi = max(maxi, point);
+//         }
+//     }
+//     return dp[day][last] = maxi;
+// }
+
+
+// int NinjaTraining(int n, vector<vector<int>> &points){        // MEMOIZATION.....
+//     vector<vector<int>> dp(n, vector<int>(4,-1));
+//     return f(n-1, 3, points, dp);
+// }
+
+
+// int NinjaTraining(int n, vector<vector<int>> &points){      // TABULATION .....TC O(n x 4 x 3) .....SC O(n x 4).
+//     vector<vector<int>> dp(n, vector<int>(4,-1));
+//     dp[0][0] = max(points[0][1], points[0][2]);
+//     dp[0][1] = max(points[0][0], points[0][2]);
+//     dp[0][2] = max(points[0][0], points[0][1]);
+//     dp[0][3] = max(points[0][1], max(points[0][2], points[0][0]));
+
+//     for(int day = 1 ; day < n ; day++){
+//         for(int last = 0 ; last < 4 ; last++){
+//             dp[day][last] = 0;
+
+//             for(int task = 0 ; task < 3 ; task++){
+//                 if(task != last){
+//                     int point = points[day][task] + dp[day-1][task];
+//                     dp[day][last] = max(dp[day][last], point);
+//                 }
+//             }
+//         }
+//     }
+//     return dp[n-1][3];
+// }
+
+
+int NinjaTraining(int n, vector<vector<int>> &points){      // TABULATION .....TC O(n x 4 x 3) .....SC O(n x 4).
+    vector<int> prev(4, 0);
+    prev[0] = max(points[0][1], points[0][2]);
+    prev[1] = max(points[0][0], points[0][2]);
+    prev[2] = max(points[0][0], points[0][1]);
+    prev[3] = max(points[0][1], max(points[0][2], points[0][0]));
+
+    for(int day = 1 ; day < n ; day++){
+        vector<int> temp(4,0);
+        for(int last = 0 ; last < 4 ; last++){
+            temp[last] = 0;
+
+            for(int task = 0 ; task < 3 ; task++){
+                if(task != last){
+                    temp[last] = max(temp[last], points[day][task] + prev[task]);
+                }
+            }
+        }
+        prev = temp;
+    }
+    return prev[3];
+}
+
+int main(){
+    vector<vector<int>> points = {{2,1,3},
+                                {3,4,6},
+                                {10,1,6},
+                                {8,3,7}};
+    int n = points.size();
+    int ans = NinjaTraining(n,points);
+    cout << "The ans is : " << ans;
+    return 0;
+}
