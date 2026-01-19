a5cc72f41bb0a13e8bd4c5ed9d1c9570008ee8b3
jeelghetia07 <jeelghetia07@gmail.com>
2026-01-19 09:30:06 +0530

    understood the most optimum sol

--- TUF DSA/16_DP/Subsequences/KnapSack_01.cpp
+++ TUF DSA/16_DP/Subsequences/KnapSack_01.cpp
@@ -28,22 +28,44 @@ using namespace std;
 
                         // TABULATION APPROACH
 
+// int knapsack(vector<int> &wt, vector<int>& val, int n, int maxWeight){
+//     vector<vector<int>> dp(n, vector<int>(maxWeight+1, 0));
+//     for(int W = wt[0] ; W <= maxWeight ; W++){      // very confusing
+//         dp[0][W] = val[0]; 
+//     }
+
+//     for(int ind = 1 ; ind < n ; ind++){
+//         for(int W = 0 ; W <= maxWeight ; W++){
+//             int nontake = 0 + dp[ind-1][W];
+//             int take = INT_MIN;
+//             if(wt[ind] <= W) take = val[ind] + dp[ind-1][W-wt[ind]];
+
+//             dp[ind][W] = max(nontake, take);
+//         }
+//     }
+//     return dp[n-1][maxWeight];
+// }
+
+
+
+                    // TABULATION APPROACH SPACE OPTIMIZED...ONE ARRAY.
+
 int knapsack(vector<int> &wt, vector<int>& val, int n, int maxWeight){
-    vector<vector<int>> dp(n, vector<int>(maxWeight+1, 0));
-    for(int W = wt[0] ; W <= maxWeight ; W++){      // very confusing
-        dp[0][W] = val[0]; 
+    vector<int> prev(maxWeight+1, 0);
+    for(int W = wt[0] ; W <= maxWeight ; W++){     
+        prev[W] = val[0]; 
     }
 
     for(int ind = 1 ; ind < n ; ind++){
-        for(int W = 0 ; W <= maxWeight ; W++){
-            int nontake = 0 + dp[ind-1][W];
+        for(int W = maxWeight ; W >= 0 ; W--){
+            int nontake = 0 + prev[W];
             int take = INT_MIN;
-            if(wt[ind] <= W) take = val[ind] + dp[ind-1][W-wt[ind]];
+            if(wt[ind] <= W) take = val[ind] + prev[W-wt[ind]];
 
-            dp[ind][W] = max(nontake, take);
+            prev[W] = max(nontake, take);
         }
     }
-    return dp[n-1][maxWeight];
+    return prev[maxWeight];
 }
 
 int main(){
