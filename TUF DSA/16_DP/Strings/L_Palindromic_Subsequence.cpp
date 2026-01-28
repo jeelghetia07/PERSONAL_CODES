3e3eeece6f890186e7099db7e319d24529199c33
jeelghetia07 <jeelghetia07@gmail.com>
2026-01-28 19:44:35 +0530

    updated

--- TUF DSA/16_DP/Strings/L_Palindromic_Subsequence.cpp
+++ TUF DSA/16_DP/Strings/L_Palindromic_Subsequence.cpp
@@ -5,9 +5,9 @@ int LCS(string& s1, string&s2){
     int n = s1.size();
     int m = s2.size();
 
-    vector<vector<int>> dp(m+1, vector<int>(m+1, 0));
+    vector<vector<int>> dp(n+1, vector<int>(m+1, 0));
 
-    for(int j = 0 ; j <= m ; j++) dp[0][j] = 0;
+    for(int j = 0 ; j <= m ; j++) dp[0][j] = 0;         // this is for the negative index base cases.
     for(int i = 0 ; i <= n ; i++) dp[i][0] = 0;
 
     for(int i = 1 ; i <= n ; i++){
@@ -18,6 +18,30 @@ int LCS(string& s1, string&s2){
             else dp[i][j] = max(dp[i-1][j], dp[i][j-1]);
         }
     }
+
+    int len = dp[n][m];     // fining the lenght of the LCS and then make a string.
+    string ans = "";
+
+    for(int i = 0 ; i < len ; i++){       // just add anything in the string.
+        ans += '$';
+    }
+
+    int index = len-1;
+    int i = n, j = m;
+
+    while(i > 0 && j > 0){
+        if(s1[i-1] == s2[j-1]){
+            ans[index] = s1[i-1];
+            index--;
+            i--, j--;
+        }
+        else if(dp[i-1][j] > dp[i][j-1]){
+            i--;
+        }
+        else j--;
+    }
+
+    cout << ans << endl; 
     return dp[n][m];
 }
 
