9e7ef14e6098890869e582c7c58e1923994439de
jeelghetia07 <jeelghetia07@gmail.com>
2026-01-31 16:36:05 +0530

    very good optimization

--- TUF DSA/16_DP/Strings/Distinct_Subsequence.cpp
+++ TUF DSA/16_DP/Strings/Distinct_Subsequence.cpp
@@ -75,23 +75,71 @@ The task is to count how many different ways we can form t from s by deleting so
 
                 // TABULATION
 
+// int numDistinct(string s1, string s2){
+//     int n = s1.size();
+//     int m = s2.size();
+//     vector<vector<int>> dp(n+1, vector<int>(m+1, -1));
+
+//     for(int i = 0 ; i <= n ; i++) dp[i][0] = 1;    
+//     for(int j = 1 ; j <= m ; j++) dp[0][j] = 0;     // this is imp...if i run the loop from j = 0...then it will modify the upper loop output.
+
+//     for(int i = 1 ; i <= n ; i++){
+//         for(int j = 1; j <= m ; j++){
+//             if(s1[i-1] == s2[j-1]){
+//                 dp[i][j] = dp[i-1][j-1] + dp[i-1][j];
+//             }
+//             else dp[i][j] = dp[i-1][j];
+//         }
+//     }
+//     return dp[n][m];
+// }
+
+
+
+
+
+                    // SPACE OPTIMIZED
+
+// int numDistinct(string s1, string s2){
+//     int n = s1.size();
+//     int m = s2.size();
+//     vector<int> prev(m+1, 0), curr(m+1, 0);
+
+//     prev[0] = curr[0] = 1;
+
+//     for(int i = 1 ; i <= n ; i++){
+//         for(int j = 1; j <= m ; j++){
+//             if(s1[i-1] == s2[j-1]){
+//                 curr[j] = prev[j-1] + prev[j];
+//             }
+//             else curr[j] = prev[j];
+//         }
+//         prev = curr;
+//     }
+//     return prev[m];
+// }
+
+
+
+
+                    // ULTRA SPACE OPTIMIZED
+
 int numDistinct(string s1, string s2){
     int n = s1.size();
     int m = s2.size();
-    vector<vector<int>> dp(n+1, vector<int>(m+1, -1));
+    vector<int> prev(m+1, 0);
 
-    for(int i = 0 ; i <= n ; i++) dp[i][0] = 1;    
-    for(int j = 1 ; j <= m ; j++) dp[0][j] = 0;     // this is imp...if i run the loop from j = 0...then it will modify the upper loop output.
+    prev[0] = 1;
 
     for(int i = 1 ; i <= n ; i++){
-        for(int j = 1; j <= m ; j++){
+        for(int j = m; j >= 0 ; j--){
             if(s1[i-1] == s2[j-1]){
-                dp[i][j] = dp[i-1][j-1] + dp[i-1][j];
+                prev[j] = prev[j-1] + prev[j];
             }
-            else dp[i][j] = dp[i-1][j];
+            else prev[j] = prev[j];
         }
     }
-    return dp[n][m];
+    return prev[m];
 }
 
 int main(){
