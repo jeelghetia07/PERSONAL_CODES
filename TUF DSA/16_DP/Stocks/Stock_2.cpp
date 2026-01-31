640c08109e107b02a5fb73654d9922d59d26f0fc
jeelghetia07 <jeelghetia07@gmail.com>
2026-01-31 16:36:16 +0530

    minor changes

--- TUF DSA/16_DP/Stocks/Stock_2.cpp
+++ TUF DSA/16_DP/Stocks/Stock_2.cpp
@@ -44,28 +44,58 @@ using namespace std;
 
                     // TABULATION
 
-int f(int ind, int buy, vector<int>& arr, int n, vector<vector<int>>& dp){
-    if(ind == n){
-        return 0;
-    }
-    if(dp[ind][buy] != -1) return dp[ind][buy];
-    int profit = 0;
 
-    if(buy){
-        profit = max(-arr[ind] + f(ind+1, 0, arr, n, dp),
-                        0 + f(ind+1, 1, arr, n, dp));
-    }
-    else{
-        profit = max(arr[ind] + f(ind+1, 1, arr, n, dp),
-                        0 + f(ind+1, 0, arr, n, dp));
-    }
-    return dp[ind][buy] = profit;
-}
+// int maxProfit(vector<int>& arr){
+//     int n = arr.size();
+//     vector<vector<int>> dp(n+1, vector<int>(2, 0));
+
+//     dp[n][0] = dp[n][1] = 0;
+
+//     for(int ind = n-1 ; ind >= 0 ; ind--){
+//         for(int buy = 0 ; buy <= 1 ; buy++){
+//             int profit = 0;
+//             if(buy){
+//                 profit = max(-arr[ind] + dp[ind+1][0],
+//                                 0 + dp[ind+1][1]);
+//             }
+//             else{
+//                 profit = max(arr[ind] + dp[ind+1][1],
+//                                 0 + dp[ind+1][0]);
+//             }
+//             dp[ind][buy] = profit;
+//         }
+//     }
+//     return dp[0][1];
+// }
+
+
+
+
+
+                        // SPACE OPTIMIZED
 
 int maxProfit(vector<int>& arr){
     int n = arr.size();
-    vector<vector<int>> dp(n, vector<int>(2, -1));
-    return f(0, 1, arr, n, dp);
+    vector<int> ahead(2, 0), curr(2, 0);
+
+    ahead[0] = ahead[1] = 0;
+
+    for(int ind = n-1 ; ind >= 0 ; ind--){
+        for(int buy = 0 ; buy <= 1 ; buy++){
+            int profit = 0;
+            if(buy){
+                profit = max(-arr[ind] + ahead[0],
+                                0 + ahead[1]);
+            }
+            else{
+                profit = max(arr[ind] + ahead[1],
+                                0 + ahead[0]);
+            }
+            curr[buy] = profit;
+        }
+        ahead = curr;
+    }
+    return ahead[1];
 }
 
 int main(){
