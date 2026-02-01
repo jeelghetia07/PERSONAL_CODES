708d166862312439f2b46aa0d03ddc48c7bfee8a
jeelghetia07 <jeelghetia07@gmail.com>
2026-02-01 10:38:25 +0530

    good que

--- TUF DSA/16_DP/Stocks/Stock_3.cpp
+++ TUF DSA/16_DP/Stocks/Stock_3.cpp
@@ -49,6 +49,17 @@ using namespace std;
 //     int n = arr.size();
 //     vector<vector<vector<int>>> dp(n+1, vector<vector<int>>(2, vector<int>(3, 0)));
 
+        // for(int ind = 0 ; ind <= n ; ind++){         // base cases.
+        //     for(int buy = 0 ; buy <= 1 ; buy++){
+        //         dp[ind][buy][0] = 0;
+        //     }
+        // }
+        // for(int buy = 0 ; buy <= 1 ; buy++){
+        //     for(int cap = 0 ; cap <= 2 ; cap++){
+        //         dp[n][buy][cap] = 0;
+        //     }
+        // }
+
 //     for(int ind = n-1 ; ind >= 0 ; ind--){
 //         for(int buy = 0 ; buy <= 1 ; buy++){
 //             for(int cap = 1 ; cap <= 2 ; cap++){
