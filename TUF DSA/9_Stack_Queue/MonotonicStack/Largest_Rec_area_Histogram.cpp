5c50bccb001300180b12d51b57b17e34af3ab1b2
jeelghetia07 <jeelghetia07@gmail.com>
2026-02-10 00:57:31 +0530

    very very intresting logic

--- TUF DSA/9_Stack_Queue/MonotonicStack/Largest_Rec_area_Histogram.cpp
+++ TUF DSA/9_Stack_Queue/MonotonicStack/Largest_Rec_area_Histogram.cpp
@@ -4,31 +4,31 @@ using namespace std;
 
                 // OPTIMAL APPROACH
 
-    int largestRectangleArea(vector<int>& arr) {
-        int n = arr.size();
-        stack<int> st;          // we are storing the indexes.
-        int maxAns = INT_MIN;
+int largestRectangleArea(vector<int>& arr) {
+    int n = arr.size();
+    stack<int> st;          // we are storing the indexes.
+    int maxAns = INT_MIN;
 
-        for(int i = 0 ; i < n ; i++){
-            while(!st.empty() && arr[st.top()] >= arr[i]){
-                int element = st.top();
-                st.pop();
-                int nse = i;        // as we only pop when we find a smaller element than the top....so obv the nse will be the i (which is smaller than top)....
-                int pse = st.empty() ? -1 : st.top();
-                maxAns = max(maxAns, arr[element] * (nse-pse-1));
-            }
-            st.push(i);
-        }
-
-        while(!st.empty()){    // if there are any elements left in the stack....for all them,....the nse will be n becoz....there are in the stack due to no further smaller elements(which can pop them out)....
-            int nse = n;
+    for(int i = 0 ; i < n ; i++){
+        while(!st.empty() && arr[st.top()] >= arr[i]){
             int element = st.top();
             st.pop();
+            int nse = i;        // as we only pop when we find a smaller element than the top....so obv the nse will be the i (which is smaller than top)....
             int pse = st.empty() ? -1 : st.top();
             maxAns = max(maxAns, arr[element] * (nse-pse-1));
         }
-        return maxAns;
+        st.push(i);
     }
+
+    while(!st.empty()){    // if there are any elements left in the stack....for all them,....the nse will be n becoz....there are in the stack due to no further smaller elements(which can pop them out)....
+        int nse = n;
+        int element = st.top();
+        st.pop();
+        int pse = st.empty() ? -1 : st.top();
+        maxAns = max(maxAns, arr[element] * (nse-pse-1));
+    }
+    return maxAns;
+}
 
             // BRUTE FORCE TC = O(3n)....SC = O(2N)+O(N)
 
