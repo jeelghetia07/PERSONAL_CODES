b15a12e699ba61219dace83a8dd720190879d24d
jeelghetia07 <jeelghetia07@gmail.com>
2026-02-11 22:37:56 +0530

    Update try.cpp

--- TUF DSA/15_Graphs/Problems_DFS_BFS/try.cpp
+++ TUF DSA/15_Graphs/Problems_DFS_BFS/try.cpp
@@ -4,10 +4,4 @@ using namespace std;
 
 
 int main(){
-    vector<vector<int>> grid = {{2, 1, 1}, 
-                          {1, 1, 0}, 
-                          {0, 1, 1}};
-
-    int ans = orangesRottenTime(grid);
-    cout << "The number of provinces are  : " << ans;
 }
