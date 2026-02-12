fef92f47fb535cd19ee56fbe2469266c3a77d280
jeelghetia07 <jeelghetia07@gmail.com>
2026-02-12 21:13:34 +0530

    Update rotten_oranges.cpp

--- TUF DSA/15_Graphs/Problems_DFS_BFS/rotten_oranges.cpp
+++ TUF DSA/15_Graphs/Problems_DFS_BFS/rotten_oranges.cpp
@@ -1,6 +1,14 @@
 #include<bits/stdc++.h>
 using namespace std;
 
+/*
+    Problem Statement: Given an n x m grid, where each cell has the following values :
+    2 - represents a rotten orange , 1 - represents a Fresh orange , 0 - represents an Empty Cell .
+
+    Every minute, if a fresh orange is adjacent to a rotten orange in 4-direction ( upward, downwards, right, and left ) it becomes rotten.
+    Return the minimum number of minutes required such that none of the cells has a Fresh Orange. If it's not possible, return -1..
+*/
+
 int orangesRottenTime(vector<vector<int>>& grid){
     if(grid.empty()) return 0;
 
