1ee8def9ef92bf5f2fe5dcdc78b9071091a61fff
jeelghetia07 <jeelghetia07@gmail.com>
2026-02-12 21:13:54 +0530

    Update No_of_distinct_Islands.cpp

--- TUF DSA/15_Graphs/Problems_DFS_BFS/No_of_distinct_Islands.cpp
+++ TUF DSA/15_Graphs/Problems_DFS_BFS/No_of_distinct_Islands.cpp
@@ -1,6 +1,10 @@
 #include<bits/stdc++.h>
 using namespace std;
 
+/*
+    Problem Statement: Given a boolean 2D matrix grid of size N x M. You have to find the number of distinct islands where a group of connected 1s (horizontally or vertically) forms an island. Two islands are considered to be distinct if and only if one island is equal to another (not rotated or reflected)
+*/
+
 class ISLANDS{
 public:
     int countDistinctIslands(vector<vector<int>> grid){
