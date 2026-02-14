cdbf941809f64bac8fbe3fa9562ce057fba15ab3
jeelghetia07 <jeelghetia07@gmail.com>
2026-02-14 13:42:40 +0530

    Update Word_Ladder.cpp

--- TUF DSA/15_Graphs/Problems_DFS_BFS/Word_Ladder.cpp
+++ TUF DSA/15_Graphs/Problems_DFS_BFS/Word_Ladder.cpp
@@ -1,6 +1,18 @@
 #include<bits/stdc++.h>
 using namespace std;
 
+/*
+    Problem Statement: Given are the two distinct words startWord and targetWord, and a list denoting wordList of unique words of equal lengths. Find the length of the shortest transformation sequence from startWord to targetWord..
+
+    In this problem statement, we need to keep the following conditions in mind:
+
+    A word can only consist of lowercase characters.
+    Only one letter can be changed in each transformation.
+    Each transformed word must exist in the wordList including the targetWord.
+    startWord may or may not be part of the wordList
+    Note:  If there’s no possible way to transform the sequence from startWord to targetWord return 0.
+*/
+
 int ladderLength(string beginWord, string endWord, vector<string>& wordList) {
     queue<pair<string, int>> q;
     q.push({beginWord, 1});     // 1 here is number of steps.
@@ -13,7 +25,7 @@ int ladderLength(string beginWord, string endWord, vector<string>& wordList) {
         int steps = q.front().second;
         q.pop();
         if(word == endWord) return steps;
-        for(int i = 0 ; i < word.size() ; i++){
+        for(int i = 0 ; i < word.size() ; i++){     // checking each char of the word with all alphabets.
             char original = word[i];
             for(char ch = 'a' ; ch <= 'z' ; ch++){
                 word[i] = ch;
