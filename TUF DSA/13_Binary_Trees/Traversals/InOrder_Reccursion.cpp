737be668d889d78a1a9d65b39dfeb808aae08b80
jeelghetia07 <jeelghetia07@gmail.com>
2026-02-21 13:57:46 +0530

    small changes

--- TUF DSA/13_Binary_Trees/Traversals/InOrder_Reccursion.cpp
+++ TUF DSA/13_Binary_Trees/Traversals/InOrder_Reccursion.cpp
@@ -16,7 +16,7 @@ public:
 class InOrder{
 public:
     void In(TreeNode* root, vector<int>& ans){
-        if(root == NULL) return ;
+        if(root == NULL) return;
         In(root->left, ans);
         ans.push_back(root->val);
         In(root->right, ans);
