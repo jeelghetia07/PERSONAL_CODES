393f182c029a1cdf9dd426783fc29b97f77fd633
jeelghetia07 <jeelghetia07@gmail.com>
2026-03-10 13:40:35 +0530

    pending

---
+++ TUF DSA/CONTESTS/Weekly_491/Min_Bitwise_OR_from_Grid.cpp
@@ -0,0 +1,22 @@
+#include<bits/stdc++.h>
+using namespace std;
+
+/*      LC = 
+    You are given a 2D integer array grid of size m x n.
+
+    You must select exactly one integer from each row of the grid.
+
+    Return an integer denoting the minimum possible bitwise OR of the selected integers from each row.
+*/
+
+int minimumOR(vector<vector<int>>& grid) {
+        
+}
+
+int main(){
+    vector<vector<int>> grid = {{1,5},{2,4}};   // ans = 1 | 2 = 3;
+    vector<vector<int>> grid = {{3,5},{6,4}};   // ans = 5 | 4 = 5;
+    vector<vector<int>> grid = {{7,9,8}};   // ans = 7;
+
+    return 0;
+}
