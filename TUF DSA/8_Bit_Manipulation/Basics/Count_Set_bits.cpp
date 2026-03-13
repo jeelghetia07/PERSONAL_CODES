407a44f9e91222326c1b12b7e6f8a050707da70f
jeelghetia07 <jeelghetia07@gmail.com>
2026-03-13 23:21:50 +0530

    Update Count_Set_bits.cpp

--- TUF DSA/8_Bit_Manipulation/Basics/Count_Set_bits.cpp
+++ TUF DSA/8_Bit_Manipulation/Basics/Count_Set_bits.cpp
@@ -1,14 +1,25 @@
 #include<bits/stdc++.h>
 using namespace std;
 
-/*      LC = 191
-    
+/*      GFG
+    count set bits in number
 */
 
+int countSETBits(int n){
+    int cnt = 0;
 
+    while(n > 0){
+        if(n & 1){
+            cnt++;
+        }
+        n = n >> 1;
+    }
+    return cnt;
+}
 
 int main(){
-
+    int n = 6;
+    cout << countSETBits(n) << endl;
 
     return 0;
 }
