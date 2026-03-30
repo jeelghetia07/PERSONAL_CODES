cde5604bd95e997b42019c5a013ffa09e7c5e394
jeelghetia07 <jeelghetia07@gmail.com>
2026-03-30 23:47:51 +0530

    very very different type que
    read the comments properly

---
+++ TUF DSA/3_Arrays/HARD/Celebrity_check.cpp
@@ -0,0 +1,56 @@
+#include<bits/stdc++.h>
+using namespace std;
+
+/*      GFG
+    A celebrity is a "person who is known to all but does not know anyone" at a party. A party is being organized by some people. A square matrix mat[][] of size n*n is used to represent people at the party such that if an element of row i and column j is set to 1 it means ith person knows jth person. You need to return the index of the celebrity in the party, if the celebrity does not exist, return -1.
+
+    Note: Follow 0-based indexing
+*/
+
+class Solution {
+  public:
+    int celebrity(vector<vector<int>>& mat) {       // here the main twist is that we have to check 2 conditions.
+                                                    // 1). A -> B (A knows B, hence A is useless) , 2). A -> B (A doesnt know B, B is useless)
+        int cand = 0;
+        int n = mat.size();
+        for(int i = 1 ; i < n ; i++){       // this will find the celebrity candidate.
+            if(mat[cand][i] == 1 && cand != i){
+                cand = i;
+            }
+        }
+        
+        for(int i = 0 ; i < n ; i++){       // for verifying that if the celebrity does not know anyone else.
+            if(mat[cand][i] == 1 && i != cand) return -1;       // here 1 means that cand -> i, hence this proves that cele knows someone, but thats practically impossible.
+        }
+        
+        for(int i = 0 ; i < n ; i++){       // for verifying that everyone knows the celebrity.
+            if(mat[i][cand] == 0 && i != cand){         // 0 means that i -> cand, i doesnt know the celebrity.
+                return -1;
+            }
+        }
+        
+        return cand;
+    }
+};
+
+int main(){
+    /*
+        Input: mat[][] =[[1, 1, 0],
+                        [0, 1, 0],
+                        [0, 1, 1]]
+        Output: 1
+        Explanation: 0th and 2nd person both know 1st person and 1st person does not know anyone. Therefore, 1 is the celebrity person.
+
+
+        Input: mat[][] = [[1, 1], 
+                        [1, 1]]
+        Output: -1
+        Explanation: Since both the people at the party know each other. Hence none of them is a celebrity person.
+
+        
+        Input: mat[][] = [[1]]
+        Output: 0
+    */
+
+    return 0;
+}
