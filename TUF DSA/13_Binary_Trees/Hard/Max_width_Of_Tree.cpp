e69cd104e7e0fec6c2eb24ad0ad08c18a8c8981a
jeelghetia07 <jeelghetia07@gmail.com>
2026-04-08 11:53:32 +0530

    op

---
+++ TUF DSA/13_Binary_Trees/Hard/Max_width_Of_Tree.cpp
@@ -0,0 +1,77 @@
+#include<bits/stdc++.h>
+using namespace std;
+
+/*      LC = 662
+    Given the root of a binary tree, return the maximum width of the given tree.
+    The maximum width of a tree is the maximum width among all levels.
+
+    The width of one level is defined as the length between the end-nodes (the leftmost and rightmost non-null nodes), where the null nodes between the end-nodes that would be present in a complete binary tree extending down to that level are also counted into the length calculation.
+
+    It is guaranteed that the answer will in the range of a 32-bit signed integer.
+*/
+
+class TreeNode{
+public:
+    int data;
+    TreeNode(int d){
+        data = d;
+    }
+    TreeNode* left = nullptr;
+    TreeNode* right = nullptr;
+};
+
+
+class Solution {
+public:
+    int widthOfBinaryTree(TreeNode* root) {
+        if(!root) return 0;
+        queue<pair<TreeNode*, int>> q;
+        q.push({root, 0});      // we are starting marking the root with index 0 and so on.
+
+        int width = 0;
+        while(!q.empty()){
+            int sz = q.size();
+            int minIdx = q.front().second;      // to subtract from all nodes on that level to form the index 0, 1, 2...etc
+            int first, last;
+
+            for(int i = 0 ; i < sz ; i++){
+                long long curr_id = q.front().second - minIdx;
+                TreeNode* node = q.front().first;
+                q.pop();
+
+                if(i == 0) first = curr_id;
+                if(i == sz-1) last = curr_id;
+
+                if(node->left) q.push({node->left, curr_id*2+1});
+                if(node->right) q.push({node->right, curr_id*2+2});
+            }
+            width = max(width, last-first+1);
+        }
+
+        return width;
+    }
+};
+
+int main(){
+    /*
+        Input: root = [1,3,2,5,3,null,9]
+        Output: 4
+        Explanation: The maximum width exists in the third level with length 4 (5,3,null,9).
+
+
+        Example 2:
+
+        Input: root = [1,3,2,5,null,null,9,6,null,7]
+        Output: 7
+        Explanation: The maximum width exists in the fourth level with length 7 (6,null,null,null,null,null,7).
+
+
+        Example 3:
+
+        Input: root = [1,3,2,5]
+        Output: 2
+        Explanation: The maximum width exists in the second level with length 2 (3,2).
+    */
+
+    return 0;
+}
