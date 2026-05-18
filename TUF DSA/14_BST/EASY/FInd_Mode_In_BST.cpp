8e948f5fbddf1d7cd247b6f0a914841b354e2237
jeelghetia07 <jeelghetia07@gmail.com>
2026-05-18 20:18:10 +0530

    nice que

---
+++ TUF DSA/14_BST/EASY/FInd_Mode_In_BST.cpp
@@ -0,0 +1,70 @@
+#include<bits/stdc++.h>
+using namespace std;
+
+/*      LC = 501
+    Given the root of a binary search tree (BST) with duplicates, return all the mode(s) (i.e., the most frequently occurred element) in it.
+
+    If the tree has more than one mode, return them in any order.
+    Assume a BST is defined as follows:
+
+    The left subtree of a node contains only nodes with keys less than or equal to the node's key.
+    The right subtree of a node contains only nodes with keys greater than or equal to the node's key.
+    Both the left and right subtrees must also be binary search trees.
+*/
+
+class TreeNode{
+public:
+    int val;
+    TreeNode(int v){
+        val = v;
+    }
+    TreeNode* left = nullptr;
+    TreeNode* right = nullptr;
+};
+
+
+class Solution {
+public:
+    int currNum = 0, currFreq = 0, maxFreq = 0;
+    vector<int> res;
+    void inorder(TreeNode* root){
+        if(root == NULL){
+            return ;
+        }
+
+        inorder(root->left);
+        
+        if(currNum == root->val){
+            currFreq++;
+        }
+        if(currNum != root->val){
+            currNum = root->val;
+            currFreq = 1;
+        }
+        if(currFreq > maxFreq){
+            res.clear();
+            maxFreq = currFreq;
+        }
+        if(currFreq == maxFreq){
+            res.push_back(root->val);
+        }
+
+        inorder(root->right);
+    }
+    vector<int> findMode(TreeNode* root) {
+        inorder(root);
+        return res;
+    }
+};
+
+int main(){
+    /*  
+        Input: root = [1,null,2,2]
+        Output: [2]
+
+        Input: root = [0]
+        Output: [0]
+    */
+
+    return 0;
+}
