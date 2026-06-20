e405b68eab1ca7d2b14766ae74b9e39d65afe2e2
jeelghetia07 <jeelghetia07@gmail.com>
2026-06-20 16:42:13 +0530

    nice logic

---
+++ TUF DSA/10_Sliding_Window/MEDIUM/Max_Points_obtained_from_cards.cpp
@@ -0,0 +1,65 @@
+#include<bits/stdc++.h>
+using namespace std;
+
+/*      LC = 1423
+    There are several cards arranged in a row, and each card has an associated number of points. The points are given in the integer array cardPoints.
+
+    In one step, you can take one card from the beginning or from the end of the row. You have to take exactly k cards.
+
+    Your score is the sum of the points of the cards you have taken.
+
+    Given the integer array cardPoints and the integer k, return the maximum score you can obtain.
+*/
+
+class Solution {
+public:
+    int maxScore(vector<int>& arr, int k) {
+        int n = arr.size();
+
+        int lSum = 0, rSum = 0;
+
+        for(int i = 0 ; i < k ; i++){
+            lSum += arr[i];
+        }
+
+        int maxScore = lSum;
+        int r = n-1;
+
+        for(int i = k-1 ; i >= 0 ; i--){
+            lSum -= arr[i];
+            rSum += arr[r--];
+
+            int total = lSum + rSum;
+
+            maxScore = max({maxScore, total});
+        }
+
+        return maxScore;
+    }
+};
+
+int main(){
+    /*
+        Example 1:
+
+        Input: cardPoints = [1,2,3,4,5,6,1], k = 3
+        Output: 12
+        Explanation: After the first step, your score will always be 1. However, choosing the rightmost card first will maximize your total score. The optimal strategy is to take the three cards on the right, giving a final score of 1 + 6 + 5 = 12.
+
+
+        Example 2:
+
+        Input: cardPoints = [2,2,2], k = 2
+        Output: 4
+        Explanation: Regardless of which two cards you take, your score will always be 4.
+
+
+        Example 3:
+
+        Input: cardPoints = [9,7,7,9,7,7,9], k = 7
+        Output: 55
+        Explanation: You have to take all the cards. Your score is the sum of points of all cards.
+    */
+
+    return 0;
+}
