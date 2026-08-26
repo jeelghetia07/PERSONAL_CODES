839670c29393e25f20e37a44e934b9a658f0e463
jeelghetia07 <jeelghetia07@gmail.com>
2026-08-26 17:32:55 +0530

    Update Buy_Sell_Stock.cpp

--- TUF DSA/3_Arrays/MEDIUM/Buy_Sell_Stock.cpp
+++ TUF DSA/3_Arrays/MEDIUM/Buy_Sell_Stock.cpp
@@ -1,7 +1,19 @@
-#include<iostream>
+#include<bits/stdc++.h>
 using namespace std;
 
-class BAS{
+class Solution {
 public:
-    void 
+    int maxProfit(vector<int>& prices) {
+        int n = prices.size();
+        int maxProfit = 0;
+        int buy = prices[0];
+
+        for(int i = 1 ; i < n ; i++){
+            if(prices[i] > buy){
+                maxProfit = max(maxProfit , prices[i]-buy);
+            }
+            buy = min(buy, prices[i]);
+        }
+        return maxProfit;
+    }
 };
